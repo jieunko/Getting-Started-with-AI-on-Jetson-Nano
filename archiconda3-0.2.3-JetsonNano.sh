051e026b717a7ca97a3e0fa0875e9bfb05f4c760
jieunko <cuytoeat@gmail.com>
2024-10-24 14:24:34 +0900

    Create archiconda3-0.2.3-JetsonNano.sh

---
+++ archiconda3-0.2.3-JetsonNano.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+uname -a
+wget https://github.com/Archiconda/build-tools/releases/download/0.2.3/Archiconda3-0.2.3-Linux-aarch64.sh
+sudo chmod 755 Archiconda3-0.2.3-Linux-aarch64.sh
+
+ ./Archiconda3-0.2.3-Linux-aarch64.sh 
